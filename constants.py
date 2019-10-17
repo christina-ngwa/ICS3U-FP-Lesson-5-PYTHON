cd25d07c4e0415047fcec4525073871206b8b2b3
Ubuntu <ubuntu@ip-172-31-20-243.ec2.internal>
2019-10-17 01:57:26 +0000

    1

---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Christina Ngwa
+# Created on: October 2019
+# This constants file is fore Space Alien game
+
+# CircuitPython screen size is 16x120 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 120
+SCREEN_GRID_X = 16
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUM_OF_ALIENS = 5
+FPS = 60
